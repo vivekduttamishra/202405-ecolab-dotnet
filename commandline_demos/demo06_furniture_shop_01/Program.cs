eabcb45bedeab8b0892aef6f79d44f6f8cd2c517
Vivek Dutta Mishra <vivek@conceptarchitect.in>
2024-05-03 11:03:10 +0530

    day#2 update#2

--- commandline_demos/demo06_furniture_shop_01/Program.cs
+++ commandline_demos/demo06_furniture_shop_01/Program.cs
@@ -4,6 +4,15 @@ class Program
     {
         System.Console.WriteLine("Welcome to Furniture Shop");
         Chair c=new Chair();
+        List l=new List();
         System.Console.WriteLine($"The price of {c} is {c.Price}");
+        for(int i=0; i<5;i++)
+        {
+            l.Add();
+        }
+
+        Table table=new Table();
+        System.Console.WriteLine($"The price of {table} is {table.Price}");
+        
     }
 }
