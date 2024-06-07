9776d7d8e4af31b37ebef2df175eb71c10eae49f
Vivek Dutta Mishra <vivek@conceptarchitect.in>
2024-06-07 13:01:48 +0530

    Day21 update2

---
+++ vs_projects/BookManagementSystem/ConceptArchitect.BookManagement.EFRepository/BooksContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConceptArchitect.BookManagement.EFRepository
+{
+    public class BooksContext: DbContext
+    {
+        public DbSet<Author> Authors { get; set; }
+    }
+}
